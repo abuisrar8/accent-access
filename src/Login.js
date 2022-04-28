81c91857d4bdd9624de228178fdb7be165b69ecb
ISRAR <abu.israr.khan.9@gmail.com>
2022-04-28 19:07:23 +0530

    update login

--- src/Login.js
+++ src/Login.js
@@ -16,8 +16,8 @@ function LogIn() {
           <h1>Welcome</h1>
           <h2>Please Log in By clicking the Log in Button </h2>
 
-          {/* <button onClick={() => setState(true)}>Log In</button> */}
-          <div className="signin">
+           <button onClick={() => setState(true)}>Log In</button> 
+          {/*<div className="signin">
           <GoogleLogin
             clientId="984245244691-9h595ughd43incve5578o6lum8nnmcck.apps.googleusercontent.com"
             buttonText="Login"
@@ -25,7 +25,7 @@ function LogIn() {
            
             cookiePolicy={'single_host_origin'}
           />
-          </div>
+          </div>*/}
         </div>
       )}
       
