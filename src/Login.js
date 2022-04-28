9de8e242b9861374a26cc22c137c3f08c7bfb824
Israr <abu.israr.khan.9@gmail.com>
2022-04-28 20:52:18 +0530

    UI fixed

--- src/Login.js
+++ src/Login.js
@@ -25,9 +25,9 @@ function LogIn() {
       {state ?  <Main /> : (
         <div className="LoginPage">
           <h1>Welcome</h1>
-          <h2>Please Log in By clicking the Log in Button </h2>
+          <h2>Sign In With Google </h2>
 
-          <button onClick={signInWithGoogle}>Log In</button>
+          <button onClick={signInWithGoogle}>Sign In </button>
           {/* <div className="signin">
           <GoogleLogin
             clientId="984245244691-9h595ughd43incve5578o6lum8nnmcck.apps.googleusercontent.com"
